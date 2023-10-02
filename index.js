2408ebcbd1102166dedead3d608cef5aa13f0bb5
Andrew Melnykx <andreymelnyk777@gmail.com>
2023-10-02 14:17:20 +0200

    fructs code

---
+++ index.js
@@ -0,0 +1,11 @@
+ userName = 'Andrew';
+ userAge = 19;
+ userHeight=170 ; 
+console.log(userHeight);
+console.log(`2 + 2 = ${2+2}`) // 2 + 2 = 4
+
+console.log(152*898)//
+
+let a = 1;
+let b = a; 
+console.log(b); // 1
---
+++ indexx.js
@@ -0,0 +1,46 @@
+console.log(5000 % 744)
+ console.log(12 ** 12)
+  console.log(878+156)
+   console.log(14*193)
+    console.log(566/28)
+     console.log(2873-493)
+        let counter = 2;
+        counter++;
+        console.log( counter )
+
+        const a = 1;
+        const b = '9';
+        console.log(a + b);
+
+        const s = 2;
+        const v = '17'
+
+        console.log(Number(v) + Number(s));
+
+       
+        let x = 30;
+        x = -x;
+        console.log(x);
+
+        let age = '122';
+console.log(+age);
+
+ let myNickName = "smuck"
+ let o =187
+console.log(myNickName + o)
+
+const favouriteColor = 'white'
+ const hateThisColor = 'red'
+console.log(hateThisColor+favouriteColor)
+
+let booleanCreak= true
+console.log(o+booleanCreak)
+
+let f
+let y = 'cocke'
+console.log(f+y)
+
+let c
+let p =NaN
+
+console.log(c+p)
